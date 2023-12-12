5e01193cf8261ef7e2abaac91d7e6433996ddf1f
ecesaruhan <148263584+ecesaruhan@users.noreply.github.com>
2023-12-12 11:33:07 +0300

    TodoApp_JS project was completed.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const addNewTask = document.querySelector('#add-new-task');
 const tasksList = document.querySelector('#tasks-list');
 const filters = document.querySelectorAll('#filters span');
@@ -12,7 +13,7 @@ const addTask = (e) => {
     e.preventDefault();
     let addedNewTaskValue = addNewTask.value.trim();
     if (addedNewTaskValue) {
-        let id = tasksListArray == 0 ? 1 : tasksListArray[tasksListArray.length - 1].id + 1;
+        let id = tasksListArray.length == 0 ? 1 : tasksListArray[tasksListArray.length - 1].id + 1;
         tasksListArray.push(
             {
                 "id": id,
@@ -29,38 +30,7 @@ const addTask = (e) => {
     addNewTask.focus();
 }
 
-const displayTasks = (filterMode) => {
-    tasksList.innerHTML = "";
-    if (tasksListArray.filter((taskItem) => {
-        if (taskItem.status == filterMode) return true;
-    }).length == 0 && filterMode != "all") {
-        tasksList.innerHTML = `<div class='alert alert-warning mb-0 text-danger'>${filterMode == "pending" ? "Pending" : "Completed"} task is not found.</div>`;
-        return;
-    } else if (tasksListArray.length == 0) {
-        tasksList.innerHTML = `<div class='alert alert-warning mb-0 text-danger'>Task is not found.</div>`;
-        return;
-    } else {
-        for (const taskItem of tasksListArray) {
-            if (filterMode == "all" || filterMode == taskItem.status) {
-                let completed = taskItem.status == "completed" ? "text-decoration-line-through" : "";
-                let taskLiElement = `
-                  <li class="list-group-item" id="${taskItem.id}">
-                           <div class="form-check d-flex justify-content-between align-items-center" >
-                               <input onclick="updateTaskStatus(this);" class="form-check-input" type="checkbox" role="checkbox" id="${taskItem.id}" ${taskItem.status == "completed" ? "checked" : ""}>
-                               
-                               <div class="input-group">
-                                   <input type="text" class="form-control ${completed}" value="${taskItem.text}" id="${taskItem.id}" disabled>
-                                   <button onclick="" class="btn btn-warning fs-4" id="${taskItem.id}"><i class="fa-regular fa-pen-to-square text-white"></i></button>
-                                   <button onclick="deleteTask(${taskItem.id})" class="btn btn-danger fs-4" id="${taskItem.id}"><i class="fa-regular fa-trash-can text-white"></i></button>
-                               </div>
-                           </div>
-                       </li>
-    `;
-                tasksList.insertAdjacentHTML("beforeend", taskLiElement);
-            }
-        }
-    }
-}
+
 
 const updateTaskStatus = (element) => {
     const newStatus = element.checked ? "completed" : "pending";
@@ -80,9 +50,88 @@ const clearAll = () => {
     displayTasks(filterMode);
 }
 
-const deleteTask = (id) = {
-    //BURADA KALDIM. SIKINTI VAR.
+const deleteTask = (id) => {
+    for (let i = 0; i < tasksListArray.length; i++) {
+        if (id == tasksListArray[i].id) {
+            tasksListArray.splice(i, 1);
+            setLocalStorage();
+            displayTasks(filterMode);
+            break;
+        }
+    }
+}
 
+const editTask = (clickedButton) => {
+    editedTaskId = clickedButton.id;
+    let editedTask = clickedButton.previousElementSibling;
+    let checkedstatus;
+    for (const taskItem of tasksListArray) {
+        if (taskItem.id == editedTaskId) {
+            checkedstatus = taskItem.status;
+            break;
+        }
+    }
+
+    if (!isEditMode) {
+        editedTask.removeAttribute("disabled");
+        if (checkedstatus == "completed") editedTask.classList.remove("text-decoration-line-through");
+        clickedButton.classList.remove("btn-warning");
+        clickedButton.classList.add("btn-success");
+        clickedButton.innerHTML = `<i class="fa-regular fa-square-check text-white"></i>`;
+        editedTask.focus();
+        isEditMode = true;
+
+    }
+    else {
+        editedTask.setAttribute("disabled", "disabled");
+        if (checkedstatus == "completed") editedTask.classList.add("text-decoration-line-through");
+        clickedButton.classList.remove("btn-success");
+        clickedButton.classList.add("btn-warning");
+        clickedButton.innerHTML = `<i class="fa-regular fa-pen-to-square text-white"></i>`
+
+        for (const taskItem of tasksListArray) {
+            if (editedTaskId == taskItem.id) {
+                taskItem.text = editedTask.value;
+                break;
+            }
+        }
+        setLocalStorage();
+        isEditMode = false;
+        displayTasks(filterMode);
+    }
+}
+
+const displayTasks = (filterMode) => {
+    tasksList.innerHTML = "";
+    if (tasksListArray.filter((taskItem) => {
+        if (taskItem.status == filterMode) return true;
+    }).length == 0 && filterMode != "all") {
+        tasksList.innerHTML = `<div class='alert alert-warning mb-0 text-danger'>${filterMode == "pending" ? "Pending" : "Completed"} task is not found.</div>`;
+        return;
+    } else if (tasksListArray.length == 0) {
+        tasksList.innerHTML = "<div class='alert alert-warning mb-0 text-danger'>Task is not found.</div>";
+        return;
+    } else {
+        for (const taskItem of tasksListArray) {
+            if (filterMode == "all" || filterMode == taskItem.status) {
+                let completed = taskItem.status == "completed" ? "text-decoration-line-through" : "";
+                let taskLiElement = `
+                  <li class="list-group-item" id="${taskItem.id}">
+                           <div class="form-check d-flex justify-content-between align-items-center" >
+                               <input onclick="updateTaskStatus(this);" class="form-check-input" type="checkbox" role="checkbox" id="${taskItem.id}" ${taskItem.status == "completed" ? "checked" : ""}>
+                               
+                               <div class="input-group">
+                                   <input type="text" class="form-control ${completed}" value="${taskItem.text}" id="${taskItem.id}" disabled>
+                                   <button onclick="editTask(this);" class="btn btn-warning fs-4" id="${taskItem.id}"><i class="fa-regular fa-pen-to-square text-white"></i></button>
+                                   <button onclick="deleteTask(${taskItem.id})" class="btn btn-danger fs-4" id="${taskItem.id}"><i class="fa-regular fa-trash-can text-white"></i></button>
+                               </div>
+                           </div>
+                       </li>
+    `;
+                tasksList.insertAdjacentHTML("beforeend", taskLiElement);
+            }
+        }
+    }
 }
 
 const getFromLocalStorage = () => {
